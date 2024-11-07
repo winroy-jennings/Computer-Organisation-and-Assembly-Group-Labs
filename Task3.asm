937ad35a8c15a06fe9148ef48352cf7ca1ff30c2
SpectroXRL <spacespectrumdev@gmail.com>
2024-11-07 12:42:54 -0500

    Added Task 3

---
+++ Task3.asm
@@ -0,0 +1,10 @@
+; Names:
+;   Renae Beckford
+;   Enoch Omoregie
+;   Kemar Knight
+;   Winroy Jennings
+
+; Accept 3 inputs: IC, CPI, CT or CR
+; CT = 1/CR
+; CR = num
+; multiplication of float values
